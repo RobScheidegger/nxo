2cb1268ed6b4dc7ec9d2e65762ab3f4873b91e73
eriqdaswan <erik@freehawk.net>
2021-05-25 16:59:55 -0400

    Styling overhaul + Added test for 3D at test2

--- Server/Services/DebugDataService.cs
+++ Server/Services/DebugDataService.cs
@@ -52,6 +52,35 @@ namespace NXO.Server.Services
                     }
                 }
             };
+
+            var testGame2 = new Game()
+            {
+                LobbyCode = "test2",
+                DateCreated = DateTime.Now,
+                GameType = "tictactoe",
+                HostPlayerId = "player1",
+                Nickname = "Test Game",
+                Stage = "In Game",
+                Settings = new GameSettings()
+                {
+                    MaximumPlayers = 2,
+                    MinimumPlayers = 2
+                },
+                Players = new Player[]
+                {
+                    new Player()
+                    {
+                        Id = "player1",
+                        Nickname = "Test Player 1"
+                    },
+                    new Player()
+                    {
+                        Id = "player2",
+                        Nickname = "Test Player 2"
+                    }
+                }
+            };
+
             await games.Add(testGame.LobbyCode, testGame);
             await tictactoe.CreateLobbyAsync(testGame);
             var settings = new TicTacToeSettings()
@@ -62,6 +91,17 @@ namespace NXO.Server.Services
             };
             await tictactoe.SaveSettingsAsync(settings);
             await tictactoe.StartGame(testGame.LobbyCode);
+
+            await games.Add(testGame2.LobbyCode, testGame2);
+            await tictactoe.CreateLobbyAsync(testGame2);
+            var settings2 = new TicTacToeSettings()
+            {
+                BoardSize = 3,
+                Dimensions = 3,
+                LobbyCode = testGame2.LobbyCode
+            };
+            await tictactoe.SaveSettingsAsync(settings2);
+            await tictactoe.StartGame(testGame2.LobbyCode);
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
