b922a31df30c0412a3be2af0dfed121baa631a1a
CannoliCatfish <CosmicDarkrai@gmail.com>
2021-06-01 12:54:10 -0400

    Added alpha-beta pruning and random move selection

--- Server/Modules/TicTacToe/TicTacToeBot.cs
+++ Server/Modules/TicTacToe/TicTacToeBot.cs
@@ -9,6 +9,10 @@ namespace NXO.Server.Modules.TicTacToe
     public class TicTacToeBot
     {
         private readonly TicTacToeGameLogicHandler logic;
+        private readonly Random random = new();
+        private readonly int A = -1000;
+        private readonly int B = 1000;
+
         public TicTacToeBot(TicTacToeGameLogicHandler logic)
         {
             this.logic = logic;
@@ -20,6 +24,7 @@ namespace NXO.Server.Modules.TicTacToe
             TicTacToeBoard board = GameStatus.Board;
             Array a = logic.GetArrayFromBoard(board);
             TicTacToePlayer currentPlayer = GameStatus.Players.Where(p => p.PlayerId == GameStatus.CurrentPlayerId).First();
+            TicTacToePlayer opp = GameStatus.Players.Where(p => p.PlayerId != currentPlayer.PlayerId).First();
             var available_moves = logic.GetPositionFromBoardWhere(a, (path, arr) => arr.GetValue(path) is null, board.Dimension);
             if (!available_moves.Any())
             {
@@ -30,21 +35,26 @@ namespace NXO.Server.Modules.TicTacToe
                 moves.Add(move);
                 Array testBoard = logic.CloneBoard(a);
                 testBoard.SetValue(currentPlayer.Token, move.ToArray());
-                TicTacToePlayer opp = GameStatus.Players.Where(p => p.PlayerId != currentPlayer.PlayerId).First();
-                scores.Add(Minimax(testBoard, 0, GameStatus, opp));
+                scores.Add(Minimax(testBoard, 0, GameStatus, opp, A, B));
+            }
+
+            var searchIndex = random.Next(0, scores.LastIndexOf(scores.Max()));
+            if (searchIndex == -1)
+            {
+                searchIndex = 0;
             }
             TicTacToeMove bestMove = new()
             {
                 PlayerId = GameStatus.CurrentPlayerId,
                 LobbyCode = GameStatus.LobbyCode,
-                Path = moves[scores.IndexOf(scores.Max())]
+                Path = moves[scores.IndexOf(scores.Max(), searchIndex)]
             };
             return Task.FromResult(bestMove);
         }
 
-        public int Minimax(Array board, int depth, TicTacToeGameStatus GameStatus, TicTacToePlayer currentPlayer)
+        public int Minimax(Array board, int depth, TicTacToeGameStatus GameStatus, TicTacToePlayer currentPlayer, int alpha, int beta)
         {
-            if (depth >= 4)
+            if (depth >= 10)
             {
                 return 0;
             }
@@ -70,23 +80,33 @@ namespace NXO.Server.Modules.TicTacToe
 
             if (currentPlayer.Bot) // MAX
             {
-                bestVal = -1000;
+                bestVal = A;
                 foreach (var move in available_moves)
                 {
                     Array testBoard = logic.CloneBoard(board);
                     testBoard.SetValue(currentToken, move.ToArray());
-                    bestVal = Math.Max(bestVal, Minimax(testBoard, depth + 1, GameStatus, opp));
+                    bestVal = Math.Max(bestVal, Minimax(testBoard, depth + 1, GameStatus, opp, alpha, beta)); ;
+                    alpha = Math.Max(alpha, bestVal);
+                    if (beta <= alpha)
+                    {
+                        break;
+                    }
                 }
 
             }
             else // MIN
             {
-                bestVal = 1000;
+                bestVal = B;
                 foreach (var move in available_moves)
                 {
                     Array testBoard = logic.CloneBoard(board);
                     testBoard.SetValue(currentToken, move.ToArray());
-                    bestVal = Math.Min(bestVal, Minimax(testBoard, depth + 1, GameStatus, opp));
+                    bestVal = Math.Min(bestVal, Minimax(testBoard, depth + 1, GameStatus, opp, alpha, beta));
+                    beta = Math.Min(beta, bestVal);
+                    if (beta <= alpha)
+                    {
+                        break;
+                    }
                 }
             }
             return bestVal;
